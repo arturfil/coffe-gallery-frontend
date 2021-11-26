021e78704826e8d46defa320cdf8c71598881121
arturfil <arturfil@hotmail.com>
2021-11-26 11:51:41 -0500

    added better search and loader

--- src/components/CoffeeCard.js
+++ src/components/CoffeeCard.js
@@ -2,17 +2,24 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import './CoffeeCard.css'
+import "./CoffeeCard.css";
 
-const coffeeImage = "https://static.wixstatic.com/media/c4bc6c_bb5653f74f4c4c5a85bfd539d8b61aa2~mv2.png/v1/fill/w_1000,h_996,al_c,usm_0.66_1.00_0.01/c4bc6c_bb5653f74f4c4c5a85bfd539d8b61aa2~mv2.png"
+const coffeeImage =
+  "https://static.wixstatic.com/media/c4bc6c_bb5653f74f4c4c5a85bfd539d8b61aa2~mv2.png/v1/fill/w_1000,h_996,al_c,usm_0.66_1.00_0.01/c4bc6c_bb5653f74f4c4c5a85bfd539d8b61aa2~mv2.png";
 
-const CoffeeCard = ({obj}) => {
+const CoffeeCard = ({ obj }) => {
   return (
     <Card style={{ width: "15rem" }}>
       <Card.Body>
         <Card.Title>{obj.name}</Card.Title>
-        <img className="coffeeCardImage" src={obj.image ? obj.image : coffeeImage} alt="" />
-        <Link to={`/coffee/${obj._id}`} className="buttonCard btn btn-primary">View More</Link>
+        <img
+          className="coffeeCardImage"
+          src={obj.image ? obj.image : coffeeImage}
+          alt=""
+        />
+        <Link to={`/coffee/${obj._id}`} className="buttonCard btn btn-primary">
+          View More
+        </Link>
       </Card.Body>
     </Card>
   );
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import CoffeeCard from "../components/CoffeeCard";
 import { getAllCoffees } from "../services/coffeeService";
 
 const HomeView = () => {
   const [coffees, setCoffees] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getCoffee();
   }, []);
 
   const getCoffee = async () => {
-    const response = await getAllCoffees();
-    setCoffees(response.data);
+    setLoading(true);
+    setTimeout(async () => {
+
+      const response = await getAllCoffees();
+      setCoffees(response.data);
+      setLoading(false);
+    }, 2000)
   };
 
-  const handleSearch = (event) => {
-    if (search === "") getCoffee();
-    setSearch(event.target.value);
-  }
+  const handleSearch = (event) => setSearch(event.target.value.toLowerCase());
 
   const resetCoffees = () => {
     getCoffee();
@@ -45,9 +49,16 @@ const HomeView = () => {
         </div>
       </div>
       <div className="container">
-        {coffees.length === 0 && <h2>No Coffees Results</h2>}
+        {loading && (
+          <div style={{textAlign: 'center', marginTop: 20}}>
+            <Spinner style={{height: 80, width: 80, fontWeight: 'bold'}} animation="border"/>
+            <h4>Loading...</h4>
+          </div>
+        )}
+        {coffees === null && <h2>No Coffees Results</h2>}
         <div className="row coffeeCards">
-          {coffees.filter(coffee => search ? coffee.name.includes(search) : coffee ).map((coffee) => (
+          {/* We are filtering at the same time we are maping that way we get in-time search results */}
+          {coffees.filter(coffee => search ? coffee.name.toLowerCase().includes(search) : coffee ).map((coffee) => (
             <div
               key={coffee._id}
               className="cardBox col-lg-3 col-md-4 col-sm-6 col-xs-12"
